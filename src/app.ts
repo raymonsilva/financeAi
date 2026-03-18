7c667b37dfb884d3b20dd64b5e1c3f5c866ad33a
Raymon Silva <raymong888@gmail.com>
2026-03-18 17:10:33 -0300

    Adding Validate import at app

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express, { Request, Response } from "express";
 import { errorMiddleware } from "./middlewares/error.middleware";
+import validate from "./middlewares/valitade.middleware";
 
 const app = express();
 
 app.use(express.json());
+app.use(validate);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Server is running!");
