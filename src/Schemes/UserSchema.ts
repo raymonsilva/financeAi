2191c87ef8dc5283fedb0a075914a6b295c5f46b
Raymon Silva <raymong888@gmail.com>
2026-03-24 14:06:58 -0300

    Setting up JWT authentication

--- src/Schemes/UserSchema.ts
+++ src/Schemes/UserSchema.ts
@@ -1,8 +1,16 @@
 import mongoose from "mongoose";
 import { z } from "zod";
+import bcrypt from "bcrypt";
 
 // Validação Zod
 export const userValidationSchema = z.object({
+    email: z.string(),
+    password: z.string().min(8, { message: "A senha deve conter pelo menos 8 Caracteres"}).
+    max(20, { message: "A senha não pode passar de 20 Caracteres"})
+    .regex(/[a-z]/, { message: "A senha deve conter pelo menos uma letra minuscula"})
+    .regex(/[A-Z]/, { message: "A senha deve conter pelo menos uma letra maiuscula"})
+    .regex(/[0-9]/, { message: "A senha deve conter pelo menos um numero"})
+    .regex(/[^a-zA-Z0-9]/, { message: "A senha deve conter pelo menos um caractere especial."}),
     nome: z.string(),
     salario: z.number().positive("Valor deve ser positivo")
 });
@@ -11,6 +19,14 @@ export type User = z.infer<typeof userValidationSchema>;
 
 // Schema Mongoose para armazenar dados
 const UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
     nome: {
         type: String,
         required: true
@@ -20,5 +36,21 @@ const UserSchema = new mongoose.Schema({
         required: true
     }
 });
+
+UserSchema.pre('save', async function(next) {
+    const user = this as any;
+
+    if (!user.isModified('password')) return;
+
+    const hash = await bcrypt.hash(user.password, 10);
+    
+    user.password = hash;
+
+});
+
+UserSchema.methods.comparePassword =  async function(candidatePassword: string): Promise<boolean> {
+    const user = this as any;
+    return bcrypt.compare(candidatePassword, user.password);
+};
 
 export const UserModel = mongoose.model('User', UserSchema);
